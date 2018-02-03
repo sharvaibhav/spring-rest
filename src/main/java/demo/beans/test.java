afd091bbb31433dc36040bdc7bd2d34220e7ac00
abhinav-111 <joy14297@gmail.com>
2018-02-03 18:54:58 +0530

    new file created

---
+++ src/main/java/demo/beans/test.java
@@ -0,0 +1,20 @@
+package demo.beans;
+
+import java.util.ArrayList;
+
+public class test {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList <Integer> l= new ArrayList();
+		l.add(5);
+		l.add(4);
+		l.add(3);
+		l.add(2);
+		l.add(1);
+		System.out.print(l);
+
+	}
+
+}
