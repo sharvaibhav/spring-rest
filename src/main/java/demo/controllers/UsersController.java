31fd0bfff173caa97a77f3e9b1038387ae926ba1
abhinav-111 <joy14297@gmail.com>
2018-02-05 00:20:33 +0530

    added db

--- src/main/java/demo/controllers/UsersController.java
+++ src/main/java/demo/controllers/UsersController.java
@@ -1,17 +1,37 @@
 package demo.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import demo.beans.Users;
-import demo.dao.UserDao;
+import demo.domain.User;
+import demo.repo.UserRepo;
 
 @RestController
 public class UsersController {
-	@RequestMapping("/users/get")
-	public Users getUsers(){
-		UserDao userDao = new UserDao();
-		return userDao.getUserList();
+	
+	@Autowired
+	UserRepo userRepo;
+	
+	
+	@RequestMapping(method=RequestMethod.GET,value="/users/get")
+	@ResponseBody
+	public List<User> getUsers(@RequestParam("id") int id){
+		
+		User juhi = new User();
+		juhi.setId(id);
+		juhi.setName("Akansha Sharma");
+		juhi.setAddress("207 lig");
+		juhi.setAadharnumber("8888-9999-00000");
+		userRepo.save(juhi);
+		return userRepo.findAll();
+//		return userDao.getUserList();
 	}
 
 }
